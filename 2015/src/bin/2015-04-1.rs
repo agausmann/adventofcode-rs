8924bca0a94d690886941880ce6702278da1d832
Adam Gausmann <agausmann@fastmail.com>
2021-12-10 20:00:26 -0600

    Add 2015 day 4

---
+++ 2015/src/bin/2015-04-1.rs
@@ -0,0 +1,30 @@
+use std::{fmt::Write as _, io::Write as _};
+
+use aoc::parsing::lines;
+use md5::{Digest, Md5};
+
+fn main() {
+    for secret in lines() {
+        let mut buf = Vec::new();
+        let mut hex = String::new();
+        for i in 0.. {
+            buf.clear();
+            hex.clear();
+
+            write!(buf, "{}{}", secret, i).unwrap();
+
+            let mut hasher = Md5::new();
+            hasher.update(&buf);
+            let digest: [u8; 16] = hasher.finalize().into();
+
+            for b in digest {
+                write!(hex, "{:02x}", b).unwrap();
+            }
+
+            if hex.starts_with("00000") {
+                println!("{}", i);
+                break;
+            }
+        }
+    }
+}
